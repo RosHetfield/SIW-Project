c2ce41fe77681dab25b3d5ee270c57ab4ab88faa
giusrit <giuseppe.ritacco93@gmail.com>
2017-06-09 17:01:17 +0200

    prova

---
+++ Fantacalcio/src/persistence/dao/prova.java
@@ -0,0 +1,17 @@
+package persistence.dao;
+
+import model.Utente;
+import persistence.DBManager;
+import persistence.DataSource;
+import persistence.MySQLDAO;
+
+public class prova {
+
+	public static void main(String[] args) {
+		MySQLDAO sqlDAO = new MySQLDAO("horton.elephantsql.com", "xrfgprnz", "5432", "xrfgprnz", "ztrokF2S22uj8fpPDP6tj3nOFzt5dmr4");
+
+		String a=sqlDAO.getUtenteDAO().findByPrimaryKey("FC Peroni").toString();
+		System.out.println("################"+ a+"##################");
+	}
+
+}
