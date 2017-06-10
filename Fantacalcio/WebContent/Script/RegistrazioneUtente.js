064b76d8b0aeb214f7d7dfed325fa2291187050c
RosHetfield <bisignano93@yahoo.it>
2017-06-10 18:19:18 +0200

    asd

---
+++ Fantacalcio/WebContent/Script/RegistrazioneUtente.js
@@ -0,0 +1,81 @@
+
+$(document).ready(init);
+
+function Utente(username, nome, cognome, email, password) {
+	this.username = username;
+	this.nome = nome;
+	this.cognome = cognome;
+	this.email = email;
+	this.password = password;
+}
+
+function CreateUtente() {
+	var campi = $('formRegistrazione').find("input");
+	var campo = campi.eq(0);
+	var username = campo.val();
+	
+	campo = campi.eq(1);
+	var nome = campo.val();
+	
+	campo = campi.eq(2);
+	var cognome = campo.val();
+	
+	campo = campi.eq(3);
+	var email = campo.val();
+	
+	campo = campi.eq(4);
+	var password = campo.val();
+	
+	var utente = new Utente(username, nome, cognome, email, password);
+	return utente;
+}
+
+function aggiungiUtente() {
+	var utente = CreaUtente();
+	var registrazione = false;
+	
+	var jsonUtente = {
+			username : utente.username,
+			nome : utente.nome,
+			cognome : utente.cognome,
+			email : utente.email,
+			password : utente.password
+		};
+
+		$.ajax({
+			async : false,
+			type : "POST",
+			url : "Registrazione",
+			datatype : "json",
+			data : {
+				utenteRegistrazione : JSON.stringify(jsonUtente),
+			},
+
+			success : function(data) {
+				
+				swal({
+					title: "Sfaccim!",
+					  text:"Impossibile completare la banana",
+					  type: "error",
+					  confirmButtonText: "O ver"
+				});
+				registrazione = true;
+			},
+
+			error : function(data) {
+
+				
+				swal({
+					  title: "Errore!",
+					  text:"Impossibile completare la registrazione",
+					  type: "error",
+					  confirmButtonText: "Riprova"
+					});
+				registrazione = false;
+			}
+
+		});
+
+		return Boolean(registrazione);
+}
+
