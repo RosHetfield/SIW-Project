73183ad18fbd5dd3fed00e0bd039cc7410322a44
giusrit <giuseppe.ritacco93@gmail.com>
2017-06-14 20:12:39 +0200

    gg

--- Fantacalcio/src/controller/LoginController.java
+++ Fantacalcio/src/controller/LoginController.java
@@ -18,50 +18,59 @@ import persistence.DBManager;
 @WebServlet("/LoginController")
 public class LoginController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public LoginController() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#HttpServlet()
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	public LoginController() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		doPost(request, response);
 	}
 
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		if (request.getParameterNames().hasMoreElements()) {
-			 
-			String jsString = request.getParameter("utenteRegistrazione");
-			if (jsString != null) {
 
+			String jsString = request.getParameter("credenzialiOperatore");
+			if (jsString != null) {
 				ObjectMapper mapper = new ObjectMapper();
 
-				Utente utente = (Utente) mapper.readValue(
-						jsString, Utente.class);
+				Utente utente = (Utente) mapper.readValue(jsString, Utente.class);
 
-				String user= DBManager.getInstance().getUtente().findByPrimaryKey(utente.getUsername()).getUsername();
+				if (utente.getUsername() != null) {
+					Utente result = DBManager.getInstance().getUtente().findByPrimaryKey(utente.getUsername());
+					if (result != null) {
+						if (result.getPassword().equals(utente.getPassword())) {
 
-				response.setStatus(HttpServletResponse.SC_OK);
-				response.setContentType("text/html"); 
-				
-				System.out.println(utente.getUsername());
-				System.out.println(utente.getNome());
-				System.out.println(utente.getCognome());
-				System.out.println(utente.getEmail());
-				System.out.println(utente.getPassword());
+							response.getWriter().print(0);
+						} else {
+
+							response.getWriter().print(1);
+						}
+
+					} else {
+
+						response.getWriter().print(2);
+					}
+
+				} else
+					response.getWriter().print(2);
 
 			}
-			
+
 		}
 	}
-
 }
