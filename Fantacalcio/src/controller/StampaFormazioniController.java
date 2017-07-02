2adf13f3d14d533cfc53b77ec6a5028018fc4c4d
giusrit <giuseppe.ritacco93@gmail.com>
2017-07-02 21:27:41 +0200

    aa

--- Fantacalcio/src/controller/StampaFormazioniController.java
+++ Fantacalcio/src/controller/StampaFormazioniController.java
@@ -54,7 +54,7 @@ public class StampaFormazioniController extends HttpServlet {
 
 				request.setAttribute("ultimaGiornata", giornata);
 				request.setAttribute("giocatoriUltima", giocatori);
-				request.setAttribute("squadreCampionato", squadreCampionato);////////////??
+				request.setAttribute("squadreCampionato", squadreCampionato);//////////??
 				
 				for (Giocatore_in_formazione g : giocatori) {
 					System.out.println("sono il giocatore: "+ g.getNomeGiocatoreRosa()+" "+g.isTitolare());
